479a587b67cc78167cd8885c294241773aed8725
Dany <daniloluongo22@gmail.com>
2023-11-14 14:20:44 +0100

    Start

---
+++ NC1-Compass/NC1-Compass/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  NC1-Compass
+//
+//  Created by Danilo Luongo on 14/11/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ NC1-Compass/NC1-Compass/NC1_CompassApp.swift
@@ -0,0 +1,17 @@
+//
+//  NC1_CompassApp.swift
+//  NC1-Compass
+//
+//  Created by Danilo Luongo on 14/11/23.
+//
+
+import SwiftUI
+
+@main
+struct NC1_CompassApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
