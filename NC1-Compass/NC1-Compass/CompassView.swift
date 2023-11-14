74860342b6f6525bf0fffdb46b6e790c57dfc1c7
Dany <daniloluongo22@gmail.com>
2023-11-14 15:16:59 +0100

    xcbsicvs

---
+++ NC1-Compass/NC1-Compass/CompassView.swift
@@ -0,0 +1,14 @@
+//
+//  CompassView.swift
+//  NC1-Compass
+//
+//  Created by Danilo Luongo on 14/11/23.
+//
+
+import SwiftUI
+
+struct CompassView: View {
+    var body: some View {
+        Text("Mammt")
+    }
+}
---
+++ NC1-Compass/NC1-Compass/MainView.swift
@@ -0,0 +1,18 @@
+//
+//  MainView.swift
+//  NC1-Compass
+//
+//  Created by Danilo Luongo on 14/11/23.
+//
+
+import SwiftUI
+
+struct MainView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MainView()
+}
